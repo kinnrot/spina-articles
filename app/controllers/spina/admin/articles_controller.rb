2e0582eed1e069cb0f0ff8cd05ebef5fdf18e917
Chen Kinnrot <kinnrot@gmail.com>
2017-03-15 08:55:44 +0200

    add category id as allowed param

--- app/controllers/spina/admin/articles_controller.rb
+++ app/controllers/spina/admin/articles_controller.rb
@@ -50,7 +50,7 @@ module Spina
       end
 
       def article_params
-        params.require(:article).permit(:title, :body, :teaser, :author, :draft, :publish_date, :slug, :photo_id)
+        params.require(:article).permit(:title, :body, :teaser, :author, :draft, :publish_date, :slug, :photo_id, :category_id)
       end
     end
   end
